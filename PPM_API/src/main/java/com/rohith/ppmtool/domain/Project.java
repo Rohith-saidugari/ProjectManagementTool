35a309b5d7dfc358a44862aa9e12cbde33f0953b
Rohit <rohith.saidugari@outlook.com>
2020-07-02 18:18:30 -0400

    Create Project API with No Validations

--- PPM_API/src/main/java/com/rohith/ppmtool/domain/Project.java
+++ PPM_API/src/main/java/com/rohith/ppmtool/domain/Project.java
@@ -1,4 +1,40 @@
 package com.rohith.ppmtool.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String projectName;
+    private String ProjectIdentifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt=new Date();
+    }
 }
