e1368293c7d43f726b744ce9fc0bce82a9b5b561
Rohit <rohith.saidugari@outlook.com>
2020-07-03 14:02:21 -0400

    Client Commit

--- PPM_API/src/main/java/com/rohith/ppmtool/services/ProjectService.java
+++ PPM_API/src/main/java/com/rohith/ppmtool/services/ProjectService.java
@@ -21,4 +21,22 @@ public class ProjectService {
             throw new ProjectIdException("Project id "+project.getProjectIdentifier().toUpperCase()+" already exists");
         }
     }
+
+    public Project findProjectByIdentifier(String projectId){
+          Project project =   projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+          if(project == null)
+              throw new ProjectIdException("Project "+projectId+" Does not exists");
+          return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+    public void deleteProjectByIdentifier(String projectId){
+        Project project =   projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project == null)
+            throw new ProjectIdException("Cannot delete project Id "+projectId+" . The project does not exists");
+        projectRepository.delete(project);
+    }
 }
