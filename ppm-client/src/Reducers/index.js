a9b32ba717ece1d1aaec70da487d8c112134a8c0
Rohit <rohith.saidugari@outlook.com>
2020-07-05 13:02:47 -0400

    Redux implementation for add project use case ..

--- ppm-client/src/Reducers/index.js
+++ ppm-client/src/Reducers/index.js
@@ -1,6 +1,6 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 
 export default combineReducers({
-    erros:errorReducer
+  errors: errorReducer,
 });
