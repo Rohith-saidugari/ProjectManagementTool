34aee6ea6829f8547356b8dec4fa030d9c8788c5
Rohit <rohith.saidugari@outlook.com>
2020-07-05 18:02:16 -0400

    Redux Implementation for getProjects

--- ppm-client/src/Components/Dashboard.js
+++ ppm-client/src/Components/Dashboard.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
 import CreateProjectButton from "./Project/CreateProjectButton";
+import { connect } from "react-redux";
+import { getProjects } from "../Actions/projectActions";
+import PropTypes from "prop-types";
 
 class Dashboard extends Component {
+  //Get projects after mounting
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
+    const projectObject = {
+      projectName: "projectName",
+      projectIdentifier: "identifier",
+      description: "description",
+    };
+
+    const { projects } = this.props.project;
+
     return (
       <div className="projects">
         <div className="container">
@@ -11,10 +27,12 @@ class Dashboard extends Component {
             <div className="col-md-12">
               <h1 className="display-4 text-center">Projects</h1>
               <br />
-              <CreateProjectButton/>
+              <CreateProjectButton />
               <br />
               <hr />
-              <ProjectItem />
+              {projects.map((project) => (
+                <ProjectItem project={project} key={project.id} />
+              ))}
             </div>
           </div>
         </div>
@@ -22,4 +40,12 @@ class Dashboard extends Component {
     );
   }
 }
-export default Dashboard;
+
+Dashboard.propTypes = {
+  project: PropTypes.object.isRequired,
+  getProjects: PropTypes.func.isRequired,
+};
+const mapStateToProps = (state) => ({
+  project: state.project,
+});
+export default connect(mapStateToProps, { getProjects })(Dashboard);
