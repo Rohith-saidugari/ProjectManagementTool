a0a757b876d43cf3b79c298640f6c7df41d3d164
Rohit <rohith.saidugari@outlook.com>
2020-07-06 17:46:04 -0400

    Create Project Task API HappyPath implementation

---
+++ PPM_API/src/main/java/com/rohith/ppmtool/controllers/BacklogController.java
@@ -0,0 +1,34 @@
+package com.rohith.ppmtool.controllers;
+
+import com.rohith.ppmtool.domain.ProjectTask;
+import com.rohith.ppmtool.services.ProjectTaskService;
+import com.rohith.ppmtool.services.ValidationErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlogs")
+@CrossOrigin
+public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private ValidationErrorService validationErrorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addProjectTaskToBacklog(@Valid @RequestBody ProjectTask projectTask ,
+                                                     BindingResult result,@PathVariable String backlog_id){
+       if(result.hasErrors())
+           return validationErrorService.mapErrors(result);
+       ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id,projectTask);
+       return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+
+}
---
+++ PPM_API/src/main/java/com/rohith/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,35 @@
+package com.rohith.ppmtool.services;
+
+import com.rohith.ppmtool.domain.Backlog;
+import com.rohith.ppmtool.domain.ProjectTask;
+import com.rohith.ppmtool.repositories.BacklogRepository;
+import com.rohith.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier,ProjectTask projectTask){
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+        Integer backlogSequence = backlog.getPTSequence();
+        backlogSequence++;
+        backlog.setPTSequence(backlogSequence);
+        projectTask.setProjectSequence(projectIdentifier+"_"+backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+        // 1 Highest Priority 3 Lowest Priority
+        if(projectTask.getPriority() == null|| projectTask.getPriority() == 0 )
+            projectTask.setPriority(3);
+        if(projectTask.getStatus() == null || projectTask.getStatus().equals(""))
+            //TODO enums for tasks
+            projectTask.setStatus("TO_DO");
+        return projectTaskRepository.save(projectTask);
+    }
+}
