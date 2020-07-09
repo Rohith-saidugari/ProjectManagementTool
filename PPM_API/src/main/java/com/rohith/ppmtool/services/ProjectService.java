6c88a38d54c21fb8e4bcb7a78640771138110ad3
Rohit <rohith.saidugari@outlook.com>
2020-07-09 14:28:36 -0400

    Project Task Service method level security accomplihsed
    **** API  DEVELOPMENT FINISHED****

--- PPM_API/src/main/java/com/rohith/ppmtool/services/ProjectService.java
+++ PPM_API/src/main/java/com/rohith/ppmtool/services/ProjectService.java
@@ -27,6 +27,15 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project,String username){
         String projectIdentifier  = project.getProjectIdentifier().toUpperCase();
+        if(project.getId()!=null){
+          Project existingProject = projectRepository.findByProjectIdentifier(project.getProjectIdentifier());
+          if(existingProject!=null && !existingProject.getProjectLeader().equals(username)){
+              throw new ProjectNotFoundException("Project not found in your account");
+          }
+          else if(existingProject == null){
+              throw new ProjectNotFoundException("Project with id "+ project.getProjectIdentifier() +" cannot be updated because it's doesn't exist");
+          }
+        }
         try{
             User user = userRepository.findByUsername(username);
             project.setUser(user);
