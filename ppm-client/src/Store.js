76670118c9147e9aa96e37ec5f9f7a7fddb602a8
Rohit <rohith.saidugari@outlook.com>
2020-07-09 23:06:01 -0400

    Fix issue App not working in incognito mode

--- ppm-client/src/Store.js
+++ ppm-client/src/Store.js
@@ -6,17 +6,14 @@ const initialState = {};
 const middleware = [thunk];
 let store;
 
-const ReactReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
-window.__REDUX_DEVTOOLS_EXTENSION__()
+const ReactReduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 if (window.navigator.userAgent.includes("Chrome") && ReactReduxDevTools) {
   store = createStore(
     rootReducer,
     initialState,
-    compose(
-      applyMiddleware(...middleware),
-      ReactReduxDevTools
-    )
+    compose(applyMiddleware(...middleware), ReactReduxDevTools)
   );
 } else {
   store = createStore(
